564455f7f55e207e84c59d58974b548732d08145
MwaniN <brooklynewyork@gmail.com>
2025-01-08 17:45:51 -0500

    Breaking out API calls

---
+++ src/APICalls.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+
+export default function APICalls ({setQuote, setCorrectAnime, setChoices}) {
+
+   // retrive the first anime quote
+   return axios.get(`http://localhost:3000/randomQuote`
+   ).then(function (response) {
+     // console.log(response.data, " This is response.data")
+     // console.log(response.data.anime, " This is response.anime")
+   setQuote(response.data.content);
+   setCorrectAnime(response.data.anime.name);
+
+   axios.get(`http://localhost:3000/random3Anime`
+   ).then( function (response) {
+     // console.log(response, ' This is the response from the random3Anime call')
+     setChoices(response.data)
+     // console.log(choices, " This is the initial choices state Array")
+   }
+   ).catch(
+     function(error) {
+       if (error.response) {
+         // The request was made and the server responded with a status code
+         // that falls out of the range of 2xx
+         console.log(error.response.data);
+         console.log(error.response.status);
+         console.log(error.response.headers);
+       } else if (error.request) {
+         // The request was made but no response was received
+         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+         // http.ClientRequest in node.js
+         console.log(error.request);
+       } else {
+         // Something happened in setting up the request that triggered an Error
+         console.log('Error', error.message);
+       }
+       console.log(error.config);
+     }
+   )
+
+ }).catch(
+   function(error) {
+     if (error.response) {
+       // The request was made and the server responded with a status code
+       // that falls out of the range of 2xx
+       console.log(error.response.data);
+       console.log(error.response.status);
+       console.log(error.response.headers);
+     } else if (error.request) {
+       // The request was made but no response was received
+       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+       // http.ClientRequest in node.js
+       console.log(error.request);
+     } else {
+       // Something happened in setting up the request that triggered an Error
+       console.log('Error', error.message);
+     }
+     console.log(error.config);
+   }
+ )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import axios from 'axios';
 import Choices from './Choices.jsx'
+import APICalls from './APICalls.jsx'
 
 function shuffle(arr) {
   let array = arr;
@@ -26,64 +26,11 @@ function App() {
   let currentChoice = null;
 
   useEffect(() => {
-    // retrive the first anime quote
-    axios.get(`http://localhost:3000/randomQuote`
-      ).then(function (response) {
-        // console.log(response.data, " This is response.data")
-        // console.log(response.data.anime, " This is response.anime")
-      setQuote(response.data.content);
-      setCorrectAnime(response.data.anime.name);
-
-      axios.get(`http://localhost:3000/random3Anime`
-      ).then( function (response) {
-        // console.log(response, ' This is the response from the random3Anime call')
-        setChoices(response.data)
-        // console.log(choices, " This is the initial choices state Array")
-      }
-      ).catch(
-        function(error) {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-          }
-          console.log(error.config);
-        }
-      )
-
-    }).catch(
-      function(error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
-      }
-    )
+    <APICalls setQuote={setQuote} setCorrectAnime={setCorrectAnime} setChoices={setChoices}/>
   }, [])
 
   function handleClick(animeName) {
+    // Update their current choice
     currentChoice = animeName
   }
 
