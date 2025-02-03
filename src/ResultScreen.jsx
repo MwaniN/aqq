57d91efab19c659f18e61471150e1fdc8e167848
MwaniN <brooklynewyork@gmail.com>
2025-02-03 12:31:52 -0500

    Result screen begun

--- src/ResultScreen.jsx
+++ src/ResultScreen.jsx
@@ -1,8 +1,27 @@
 // include option to save quote to their favorites in the future
 
-export default function Result () {
+export default function Result ({correct, animeChoice, correctAnime}) {
+
+  // show a different screen depending on if correct is false or not
+  // have a button that moves on to the next quote
+
+  // Input: The anime choice, the correctAnime, and whether the choice was right or not
+  // Output: A screen (component) showing whether the choice was right or not. It should sit underneath the quote
+  // on the main page.
+  // Constraints: It needs to only appear after the choice was submitted and then be removed / cleared after
+  // it moves on to the next quote.
+  // Edge Cases:
 
   return <div>
-    Great, you are correct! The answer was insert anime here.
+    {
+      function(){
+        if (correct){
+          return `Correct! You chose ${animeChoice} and the answer was ${correctAnime}`
+        } else {
+          return `Incorrect! You chose ${animeChoice} and the answer was ${correctAnime}`
+        }
+      }()
+    }
+    <button type="button">Next</button>
   </div>
 }
