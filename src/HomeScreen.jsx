2b0b702a0755a62bfd90a274c793e51d32ac1eac
MwaniN <brooklynewyork@gmail.com>
2025-02-10 12:20:20 -0500

    Note

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -2,6 +2,8 @@ import {Link} from 'react-router'
 
 export default function HomeScreen({setQuoteNum}) {
 
+  // add a slider that allows you to pass some quotes when you start the quiz
+
   return <>
   <div>Choose your game length</div>
   <div>Short</div>
