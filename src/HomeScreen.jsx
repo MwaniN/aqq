2e082ac2b85cf4e08b190125300aae56d874844f
MwaniN <brooklynewyork@gmail.com>
2025-02-10 12:13:09 -0500

    Home page linking to Quiz now

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -1,13 +1,14 @@
+import {Link} from 'react-router'
 
 export default function HomeScreen({setQuoteNum}) {
 
   return <>
   <div>Choose your game length!</div>
-  <div>Quick</div>
-  <button>5</button>
+  <div>Short</div>
+  <Link to="/Quiz"><button onClick={() => setQuoteNum(5)}>5</button></Link>
   <div>Medium</div>
-  <button>10</button>
+  <Link to="/Quiz"><button onClick={() => setQuoteNum(10)}>10</button></Link>
   <div>Long</div>
-  <button>15</button>
+  <Link to="/Quiz"><button onClick={() => setQuoteNum(15)}>15</button></Link>
   </>
 }
