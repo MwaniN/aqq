4ac4626c1d6574a4f7ac0f4e73d11de4dad2d008
MwaniN <brooklynewyork@gmail.com>
2025-02-11 08:28:53 -0500

    Cleanup of unecessary props

--- src/GameOver.jsx
+++ src/GameOver.jsx
@@ -2,13 +2,24 @@ import {Link} from 'react-router'
 
 export default function GameOver({finalScore}) {
 
+  let endingArr = [`You are a regular savant.`, `You went full Super Saiyan.`, `Good job.`]
+
+  function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  let rndInt = randomIntFromInterval(0, endingArr.length - 1);
+
+  let finalQuote = endingArr[rndInt]
+
+
   return (
     <>
     <div>
       {
         function(){
           if (finalScore > 0) {
-            return <div>{`Congratulations! You got ${finalScore} correct. You are a regular savant.`}</div>
+            return <div>{`Congratulations! You got ${finalScore} correct. ${finalQuote}`}</div>
           } else {
             return <div>Better luck next time! You got 0 correct.</div>
           }
