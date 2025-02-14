00a75e6a233a5798ca96b4d447113b00fae9c64d
MwaniN <brooklynewyork@gmail.com>
2025-02-14 14:22:38 -0500

    Removed unecessary import

--- src/auth/firebaseFunctions.js
+++ src/auth/firebaseFunctions.js
@@ -1,5 +1,5 @@
 import { firebaseAuth, analytics } from './firebase.js'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,
-  GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
+  GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
