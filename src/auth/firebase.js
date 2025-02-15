054e77ced19f8de2fb352fc9ceac85261bd1ae26
MwaniN <brooklynewyork@gmail.com>
2025-02-15 12:16:17 -0500

    Importing firebase ui

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/compat/app";
 import { getAuth } from 'firebase/compat/auth';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
