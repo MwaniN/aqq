29599926371a0832aa4b3057dcaffae6621aeb02
MwaniN <brooklynewyork@gmail.com>
2025-02-19 11:53:15 -0500

    nvm, buh-bye Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,17 @@ import { useState } from 'react'
 import { Link, Route, Routes } from 'react-router'
 import HomeScreen from './HomeScreen.jsx'
 import Quiz from './Quiz.jsx'
-import SupabaseAuth from './SupabaseAuth.jsx'
 
 function App() {
 
   const [totalQuotes, setQuoteNum] = useState(null);
-
-  function handleClick() {
-    SupabaseAuth()
-  }
 
   return (
     <>
       <header id="header">
         <ul>
         <li><Link to="/"><button type="button">Home</button></Link></li>
-        <li><button type="button" onClick={() => handleClick()}>Log In</button></li>
+        <li><button type="button">Log In</button></li>
         </ul>
         <div id="title">
         Anime Quote Quiz
