dad6b1204660995c7e66398d89118aa08bcdd645
MwaniN <brooklynewyork@gmail.com>
2025-02-19 02:31:48 -0500

    Firebase, begone

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,16 @@ import { useState } from 'react'
 import {Link, Route, Routes} from 'react-router'
 import HomeScreen from './HomeScreen.jsx'
 import Quiz from './Quiz.jsx'
-import Auth from './auth/Auth.jsx'
 
 function App() {
 
   const [totalQuotes, setQuoteNum] = useState(null);
-  const [login, setLogin] = useState(false)
-
-  function handleclick() {
-    console.log("The click handler worked")
-    if (login === false) {
-      console.log("login is false")
-      return <Auth />
-    }
-
-  }
-
 
   return (
     <>
       <header id="header">
         <ul>
         <li><Link to="/"><button>Home</button></Link></li>
-        <li><button type="button" onClick={() => {handleclick()}}>Log In</button></li>
         </ul>
         <div id="title">
         Anime Quote Quiz
