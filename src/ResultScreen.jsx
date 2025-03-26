47a33bcb41a3e7d2ded26a837d311102d7855c89
MwaniN <brooklynewyork@gmail.com>
2025-03-26 07:08:30 -0400

    Removing comments

--- src/ResultScreen.jsx
+++ src/ResultScreen.jsx
@@ -2,22 +2,6 @@
 
 export default function Result ({animeChoice, correctAnime, handleNextButton, loggedIn}) {
 
-  // show a different screen depending on if correct is false or not
-  // have a button that moves on to the next quote
-
-  // Input: The anime choice, the correctAnime, and whether the choice was right or not
-  // Output: A screen (component) showing whether the choice was right or not. It should sit underneath the quote
-  // on the main page.
-  // Constraints: It needs to only appear after the choice was submitted and then be removed / cleared after
-  // it moves on to the next quote.
-  // Edge Cases:
-
-  // use resetQuote when the button to choose next is hit
-
-  // game over screen can go here too
-
-  // make score update immediately if it's correct
-
   let isRight = false;
 
   if (animeChoice === correctAnime) {
