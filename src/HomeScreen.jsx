64489abfbee7d4f71ef5195090f50c29461be201
MwaniN <brooklynewyork@gmail.com>
2025-09-23 23:46:31 -0400

    Home screen updates

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -17,13 +17,13 @@ export default function HomeScreen({setQuoteNum, setQuizLength}) {
   return <>
   <div>Choose your game length</div>
   <div>Short</div>
-  <div>5 quotes</div>
+  <div>5 Quotes</div>
   <Link to="/Quiz"><button onClick={() => { setQuoteNum(5); setQuizLength('5'); }}>{getButtonText('5')}</button></Link>
   <div>Medium</div>
-  <div>10 quotes</div>
+  <div>10 Quotes</div>
   <Link to="/Quiz"><button onClick={() => { setQuoteNum(10); setQuizLength('10'); }}>{getButtonText('10')}</button></Link>
   <div>Long</div>
-  <div>15 quotes</div>
+  <div>15 Quotes</div>
   <Link to="/Quiz"><button onClick={() => { setQuoteNum(15); setQuizLength('15'); }}>{getButtonText('15')}</button></Link>
   </>
 }
