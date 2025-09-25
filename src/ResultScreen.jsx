592a57c93c159074a320eeec03202449e063d0cb
MwaniN <brooklynewyork@gmail.com>
2025-09-25 00:01:26 -0400

    Result Screen optimization

--- src/ResultScreen.jsx
+++ src/ResultScreen.jsx
@@ -1,5 +1,7 @@
 // include option to save quote to their favorites in the future
 
+import { useMemo } from 'react';
+
 function shuffleArray(array) {
     for (let i = array.length - 1; i >= 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -15,58 +17,62 @@ export default function Result ({animeChoice, correctAnime, handleNextButton, lo
     isRight = true;
   }
 
-  // Success messages (all mentioning anime name)
-  const successMessages = [
-    "🎉 Perfect! That's from " + correctAnime,
-    "🎯 Excellent! You nailed it! That's from " + correctAnime,
-    "⭐ Great catch! That's from " + correctAnime,
-    "🔥 Amazing! You got it right! That's from " + correctAnime,
-    "💯 Spot on! That's from " + correctAnime
-  ];
+  const resultMessage = useMemo(() => {
+    // Success messages (all mentioning anime name)
+    const successMessages = [
+      "🎉 Perfect! That's from " + correctAnime,
+      "🎯 Excellent! You nailed it! That's from " + correctAnime,
+      "⭐ Great catch! That's from " + correctAnime,
+      "🔥 Amazing! You got it right! That's from " + correctAnime,
+      "💯 Spot on! That's from " + correctAnime
+    ];
 
-  // Failure messages (all mentioning anime name)
-  const failureMessages = [
-    "❌ Close! The correct answer was " + correctAnime,
-    "🤔 Not quite! It's from " + correctAnime,
-    "💭 That's a tricky one! The answer is " + correctAnime,
-    "👍 Good try! The correct answer was " + correctAnime,
-    "🤷‍♂️ Not this time! It's from " + correctAnime
-  ];
+    // Failure messages (all mentioning anime name)
+    const failureMessages = [
+      "❌ Close! The correct answer was " + correctAnime,
+      "🤔 Not quite! It's from " + correctAnime,
+      "💭 That's a tricky one! The answer is " + correctAnime,
+      "👍 Good try! The correct answer was " + correctAnime,
+      "🤷‍♂️ Not this time! It's from " + correctAnime
+    ];
 
-  // Success encouragement messages
-  const successEncouragement = [
-    "Great deduction skills!",
-    "You're getting the hang of this!",
-    "That was a tough one!",
-    "Nice work!",
-    "Excellent!",
-    "Well done!",
-    "Perfect!",
-    "Outstanding!"
-  ];
+    // Success encouragement messages
+    const successEncouragement = [
+      "Great deduction skills!",
+      "You're getting the hang of this!",
+      "That was a tough one!",
+      "Nice work!",
+      "Excellent!",
+      "Well done!",
+      "Perfect!",
+      "Outstanding!"
+    ];
 
-  // Failure encouragement messages
-  const failureEncouragement = [
-    "Don't worry, that's a hard one!",
-    "Even experts miss that one sometimes!",
-    "That's a really obscure reference!",
-    "Nice try!",
-    "That was tricky!",
-    "No worries, keep going!",
-    "You'll get the next one!"
-  ];
+    // Failure encouragement messages
+    const failureEncouragement = [
+      "Don't worry, that's a hard one!",
+      "Even experts miss that one sometimes!",
+      "That's a really obscure reference!",
+      "Nice try!",
+      "That was tricky!",
+      "No worries, keep going!",
+      "You'll get the next one!"
+    ];
 
-  // Randomly select messages
-  shuffleArray(successMessages);
-  shuffleArray(failureMessages);
-  shuffleArray(successEncouragement);
-  shuffleArray(failureEncouragement);
-
-  const selectedMessage = isRight ? successMessages[0] : failureMessages[0];
-  const selectedEncouragement = isRight ? successEncouragement[0] : failureEncouragement[0];
+    // Only shuffle the arrays we need
+    if (isRight) {
+      shuffleArray(successMessages);
+      shuffleArray(successEncouragement);
+      return successMessages[0] + " " + successEncouragement[0];
+    } else {
+      shuffleArray(failureMessages);
+      shuffleArray(failureEncouragement);
+      return failureMessages[0] + " " + failureEncouragement[0];
+    }
+  }, [isRight, correctAnime]);
 
   return <div>
-    {selectedMessage + " " + selectedEncouragement}
+    {resultMessage}
     <button type="button" className="advance-button" onClick={() => handleNextButton()}>Next</button>
   </div>
 }
